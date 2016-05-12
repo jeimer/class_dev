eff8ff5eeebf48da4ce75263e937d50ee21c8c11
Joseph Eimer <jeimer1@jh.edu>
2016-05-12 10:37:06 -0400

    added temp file cleanup

--- camera_slueth.py
+++ camera_slueth.py
@@ -44,11 +44,12 @@ def copy_images(camera_num, start_date, end_date, local_dir):
         image_count += 1
     return
 
-def make_mp4(camera_num, start_date, end_date, outfile, reduction_factor, fps):
-    ''' creates a gif from the images from start_date to end_date'''
+def make_mp4(camera_num, start_date, end_date, outfile, fps):
+    ''' creates an mp4 from the images from start_date to end_date'''
     temp_dir = tempfile.mkdtemp(dir='/home/eimer/')
     copy_images(camera_num, start_date, end_date, temp_dir)
     subprocess.call("ffmpeg -r {1} -pattern_type glob -i '{0}/*.jpg' -r {1} -c:v libx264 {2}".format(temp_dir, fps, outfile),shell=True)
+    subprocess.call("rm -r {0}".format(temp_dir), shell=True)
     return
 
 def make_gif(camera_num, start_date, end_date, outfile, reduction_factor):
