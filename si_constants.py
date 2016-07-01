5c83dd966c6433a07d820b8fabdd6df8f2baac7e
Joseph Eimer <jeimer1@jh.edu>
2016-07-01 11:06:13 -0400

    added si_constants

---
+++ si_constants.py
@@ -0,0 +1,8 @@
+#physical constants in SI units.
+
+SPEED_C = 299792458 #[m/s] speed of light
+PI_APPROX  = 3.14159265358979323846264338327950288419716939937510582
+MU_0  = 4 * PI_APPROX * 10**(-7) #[V s/ (A m)] vacuum permeability
+EPSILON_0 = 8.85418782 * 10**(-12) #[A^2 s^4/(kg m^3)] vacuum permittivity
+PLANK_H = 6.62607004 * 10**(-34) #[kg m^2/s] Planck constant
+BOLTZMANN_K = 1.38064852 * 10**(-23) #[kg m^2/(K s^2)] Boltzmann constant
