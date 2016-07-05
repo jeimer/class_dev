45cfc3dff1716df2917685786d43d5bdc0a1c5a9
Joseph Eimer <jeimer1@jh.edu>
2016-07-05 16:02:25 -0400

    created tod_ops.py for simple intra-tod operations

---
+++ tod_ops.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def data_valid_edges(tod):
+    '''function returns the indecies of the edges of a tod.data array where the tes data are non-zero.
+    tod: valid moby2 tod object.'''
+    array_on_index = np.where(tod.data[0])[0]
+    off_edge = np.where((array_on_index[1:]-1) - array_on_index[:-1])[0]
+    inner_edges = []
+    for element in off_edge:
+        inner_edges += [array_on_index[element]]
+        inner_edges += [array_on_index[element]]
+    return np.hstack((array_on_index[0],inner_edges, array_on_index[-1])
+
