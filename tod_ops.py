de67ba2af8e39ca6ca71a149245304d6f9c8e007
Joseph Eimer <jeimer1@jh.edu>
2016-07-05 22:24:37 -0400

    change data_valid_edges to return list of pairs

--- tod_ops.py
+++ tod_ops.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def data_valid_edges(tod):
-    '''function returns the indecies of the edges of a tod.data array where the tes data are non-zero.
+    '''function returns a list of index pairs indicaing the ranges where the TES data from tod.data[0] is nonzero.
     tod: valid moby2 tod object.'''
     array_on_index = np.where(tod.data[0])[0]
     off_edge = np.where((array_on_index[1:]-1) - array_on_index[:-1])[0]
@@ -9,4 +9,8 @@ def data_valid_edges(tod):
     for element in off_edge:
         inner_edges += [array_on_index[element]]
         inner_edges += [array_on_index[element+1]]
-    return np.hstack((array_on_index[0],inner_edges, array_on_index[-1]))
+    transitions = np.hstack((array_on_index[0],inner_edges, array_on_index[-1]))
+    range_pairs = []
+    for num in range(len(transitions)/2):
+        range_pairs += [transitions[num],transitions[num+1]]
+    return range_pairs
