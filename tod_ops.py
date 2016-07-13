3c6eeb6678de7edbca7805b717ea9c1bee0f80e0
Joseph Eimer <jeimer1@jh.edu>
2016-07-13 16:08:18 -0400

    fix bugs

--- tod_ops.py
+++ tod_ops.py
@@ -52,7 +52,7 @@ def single_pole_lp_filt(freqs, tau):
     tau: (float) or (array like) time constant(s) of filter [seconds]
     '''
     pole = 2.j * np.pi * freqs
-    if type(tau) == np.float64:
+    if type(tau) == np.float64 or type(tau) == float:
         return 1./(1 + tau * pole)
     else:
         tau = tau[:,np.newaxis]
