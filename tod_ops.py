49bebc1235d3aca21bf7776d044b68e3207330bd
Joseph Eimer <jeimer1@jh.edu>
2016-09-13 08:46:06 -0400

    try other tau optimization routine

--- tod_ops.py
+++ tod_ops.py
@@ -154,13 +154,13 @@ def find_tau(tes_dat, vpm_dat):
 def find_tau2(tes_dat, vpm_dat):
     bound = (0.0009, 0.01)
     if len(np.shape(tes_dat)) == 1:
-        res1 = optimize.minimize(eval_hysteresis, 0.004, args = (tes_dat, vpm_dat), bounds = bound)
+        res1 = optimize.minimize(eval_hysteresis, [0.004], args = (tes_dat, vpm_dat), bounds = bound)
         res = [res1.x]
     else:
         res = []
         num_dets = np.shape(tes_dat)[0]
         for det_num in range(num_dets):
-               res1 = optimize.minimize(eval_hysteresis, 0.004, args = (tes_dat, vpm_dat), bounds = bound)
+               res1 = optimize.minimize(eval_hysteresis, [0.004], args = (tes_dat, vpm_dat), bounds = bound)
                res += [res1.x]
 
     return np.array(res)
