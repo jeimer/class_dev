b3a58c3a696b33964ebbd5a4113f1809b60183ba
Joseph Eimer <jeimer1@jh.edu>
2016-09-14 15:03:10 -0400

    debugging eval_hysteresis

--- tod_ops.py
+++ tod_ops.py
@@ -204,6 +204,7 @@ def find_tau2(tod):
         res1 = optimize.minimize(eval_hysteresis2, [0.003], args = (tod, det_num), bounds = bound)
         print('found the best tau : ', str(res1.x))
         res += res1.x
+        print(res)
 
     return np.array(res)
 
