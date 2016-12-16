04da5ba5d01f0faa1e7c26acf5c6f69e8c8e6ba4
Joseph Eimer <jeimer1@jh.edu>
2016-12-16 09:01:12 -0500

    debug make_sparse_grid_dict2

--- tod_ops.py
+++ tod_ops.py
@@ -520,17 +520,14 @@ def make_sparse_grid_dict2(dir_paths, ang_path, skip_meas = None):
     date_string = [int(item) for item in date_string]
     tct_pairs, angles = load_sparse_grid_csv(date_string[0], date_string[1], date_string[2], ang_path)
     #remove the measurments indicated by skip_meas
-    print(type(angles))
-    print(angles)
     ct_pairs = []
-
     for index in range(len(ct_pairs)):
         if index not in skip_meas:
             ct_pairs += [tct_pairs[index]]
+    print(ct_pairs)
     angles = np.delete(angles, skip_meas, axis = 0)
-
-    print(type(angles))
     print(angles)
+
     cal_angles = wire_grid_cal_angle(angles)
     m_dict = {key:[] for key in np.unique(cal_angles)}
     path_num = 0
