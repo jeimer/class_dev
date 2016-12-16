b9732a0fc8c4f104e567fc5486a30ca8aa61f5f6
Joseph Eimer <jeimer1@jh.edu>
2016-12-16 09:02:46 -0500

    debug make_sparse_grid_dict2

--- tod_ops.py
+++ tod_ops.py
@@ -521,7 +521,7 @@ def make_sparse_grid_dict2(dir_paths, ang_path, skip_meas = None):
     tct_pairs, angles = load_sparse_grid_csv(date_string[0], date_string[1], date_string[2], ang_path)
     #remove the measurments indicated by skip_meas
     ct_pairs = []
-    for index in range(len(ct_pairs)):
+    for index in range(len(tct_pairs)):
         if index not in skip_meas:
             ct_pairs += [tct_pairs[index]]
     print(ct_pairs)
