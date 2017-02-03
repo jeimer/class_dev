79f2a319c7ca4a8a9cbf50fad6ad1d8f3adc7df8
Joseph Eimer <jeimer1@jh.edu>
2017-02-03 15:41:34 -0500

    profile demod

--- demod.py
+++ demod.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 class Demodulator(object):
@@ -17,13 +18,28 @@ class Demodulator(object):
         return
 
     def set_tw(self, tw):
+        '''
+        set the transition width of the filters
+        '''
         self._tw = tw
         return
 
     def get_tw(self):
+        '''
+        return transition width of the fiters
+        '''
         return self._tw
 
     def lpkern(self, fc, n):
+        '''
+        Impulse response of windowed sinc low-pass filter
+        Parameters:
+        fc(float): cutoff frequency as a fraction of the sampling frequency.
+        n(int): required length of the returned filter
+        Returns:
+        zero-phase windowed-sinc low-pass filter time-domain kernel of
+        length n.
+        '''
         order = int(np.ceil(2. / self._tw) * 2)
         t = np.arange(order + 1)
         t = t - order/2
@@ -37,21 +53,56 @@ class Demodulator(object):
         return np.roll(s, -order/2)
 
     def hpkern(self, fc, n):
+        '''
+        Impulse response of windowed sinc high-pass filter
+        Parameters:
+        fc(float): cutoff frequency as a fraction of the sampling frequency.
+        n(int): required length of the returned filter
+        Returns:
+        zero-phase windowed-sinc high-pass filter time-domain kernel of
+        length n.
+        '''
         hp = -self.lpkern(fc, n)
         hp[0] = hp[0] + 1
         return hp
 
     def b_stopkern(self, fcl, fch, n):
+        '''
+        Impulse response of windowed sinc band-stop filter
+        Parameters:
+        fcl(float): low-edge cutoff frequency as a fraction of the
+        sampling frequency.
+        fch(float): high-edge cutoff frequency as a fraction of the
+        sampling frequency.
+        n(int): required length of the returned filter
+        Returns:
+        zero-phase windowed-sinc band-stop filter time-domain kernel of
+        length n.
+        '''
         return self.lpkern(fcl, n) + self.hpkern(fch, n)
 
     def b_passkern(self, fcl, fch, n):
+        '''
+        Impulse response of windowed sinc band-pass filter
+        Parameters:
+        fcl(float): low-edge cutoff frequency as a fraction of the
+        sampling frequency.
+        fch(float): high-edge cutoff frequency as a fraction of the
+        sampling frequency.
+        n(int): required length of the returned filter
+        Returns:
+        zero-phase windowed-sinc band-pass filter time-domain kernel of
+        length n.
+        '''
         bp = -self.b_stopkern(fcl, fch, n)
         bp[0] = bp[0] + 1
         return bp
 
     def filt(self, kern, data, fc):
         '''
-        kern: filter func to apply to data
+        apply the specified kernal to the data with 
+        kern: filter func to apply to data. (currently only works for
+        highpass and lowpass kernels)
         data to be filtered. np array.
         fc: cuttoff freq in units of sampling freq
         '''
@@ -63,6 +114,17 @@ class Demodulator(object):
         return np.fft.irfft(f_data * f_imp, np.shape(data)[-1])
 
     def demod(self, param = 'u', fcl = 1.):
+        '''
+        demodulate the instatiated tod.
+        *** WARNING ***
+        THE TOD IS ALTERED
+        Parameters:
+        param(string): either 'u' or 'v' to demodulate
+        fcl(float): low-pass cuttoff frequency in Hz. The low pass filter is
+        applied after taking the product of the tod with the VPM transfer
+        function.
+
+        '''
         fc = fcl/self._sampling_freq
         s = {'u': self._utrans, 'v': self._vtrans}
         pos = np.digitize(self._tod.vpm - 0.0001/2, self._bins)
@@ -73,17 +135,36 @@ class Demodulator(object):
         return
 
     def demod2(self, param = 'u', fh = 7., fl = 1.):
+        '''
+        demodulate the instatiated tod.
+        *** WARNING ***
+        THE TOD IS ALTERED
+        Parameters:
+        param(string): either 'u' or 'v' to demodulate
+        fh(float): high-pass cuttoff frequency in Hz. The TOD and vpm are both
+        high-passed prior to demodulation. 
+        fcl(float): low-pass cuttoff frequency in Hz. The low pass filter is
+        applied after taking the product of the tod with the VPM transfer
+        function.
+        '''
+
         fh = fh/self._sampling_freq
         fl = fl/self._sampling_freq
         s = {'u': self._utrans, 'v': self._vtrans}
+        t = []
+        t += [time.time()]
         self._tod.data = self.filt(self.hpkern, self._tod.data, fh)
+        t += [time.time()]
         pos = np.digitize(self._tod.vpm - 0.0001/2, self._bins)
+        t += [time.time()]
         self._tod.vpm = self._tod.vpm.reshape(1, len(self._tod.vpm))
+        t += [time.time()]
         self._tod.vpm = self.filt(self.hpkern, self._tod.vpm, fh)
-
-        print('pos shape ', np.shape(pos))
+        t += [time.time()]
         self._tod.data *= s[param][:, pos]
+        t += [time.time()]
         self._tod.data = self.filt(self.lpkern, self._tod.data, fl)
-        return
+        t += [time.time()]
+        return t
 
 
